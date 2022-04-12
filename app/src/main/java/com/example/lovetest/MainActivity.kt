d26306a11eef1a7554e7975bfa511eb8348991b2
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-04-12 23:47:22 +0900

    fragment 생성, 디자인 / adv 실행해서 main 띄우기 -> Kotlin_v3에서 개념 더 다진뒤 추후 진행예정

--- app/src/main/java/com/example/lovetest/MainActivity.kt
+++ app/src/main/java/com/example/lovetest/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.lovetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.lovetest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // navigation과 activity 연결
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val
+        
+        // 바인딩 초기화
+        val binding = ActivityMainBinding.inflate(layoutInflater);
+3       // 레이아웃(root뷰) 표시
+        setContentView(binding.root);
+        // 뷰바인딩으로 버튼 접근
     }
 }
--- app/src/main/java/com/example/lovetest/fragment/MainFragment.kt
+++ app/src/main/java/com/example/lovetest/fragment/MainFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavAction
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import com.example.lovetest.R
+import com.example.lovetest.databinding.ActivityMainBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+
+lateinit var navController: NavController
 
 /**
  * A simple [Fragment] subclass.
@@ -31,11 +38,19 @@ class MainFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
     }
 
     companion object {
