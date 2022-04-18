3055f8c71826d5649f1c9c7a321e2425a8ec4d60
DESKTOP-2ASIST7\PC <kim3jjj@naver.com>
2022-04-18 19:28:15 +0900

    fragment_question -> fragment_selection 이동 구현

--- app/src/main/java/com/example/lovetest/fragment/MainFragment.kt
+++ app/src/main/java/com/example/lovetest/fragment/MainFragment.kt
@@ -6,20 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import com.example.lovetest.R
-import com.example.lovetest.databinding.FragmentMainBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class MainFragment : Fragment() {
-
-    private var mBinding : FragmentMainBinding? = null
-    private val binding get() = mBinding!!
-
-    lateinit var navController: NavController
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_main, container, false)
